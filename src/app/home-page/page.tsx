58ed4b699570d913ca5f0be630f14a021ab37087
baiocco <igor64br@gmail.com>
2025-07-16 16:35:30 -0300

    feat: botao que afunda e like pulsante

--- src/app/home-page/page.tsx
+++ src/app/home-page/page.tsx
@@ -1,12 +1,22 @@
 import NeuComponent from "./components/NeuComponent/NeuComponent";
 import "./css/style.css";
 import "./css/led-checkbox.css";
+import "./css/top-bottom-button.css"
+import LikeBtn from "./components/LikeBtn/LikeBtn";
 
 export default function HomePage() {
     return (
         <div className="body">
             <h1>Neumorphic Components</h1>
             <div className="grid-container">
+                <NeuComponent name="Animated Like Button">
+                    <LikeBtn/>
+                </NeuComponent>
+                <NeuComponent name="Top Bottom Button">
+                    <button className="top-bottom-button" type="button">
+                        <p>Click me!</p>
+                    </button>
+                </NeuComponent>
                 <NeuComponent name="Switch Button">
                     <div className="led-checkbox-container">
                         <input id="switch-checkbox" type="checkbox" name="switch-checkbox"/>
