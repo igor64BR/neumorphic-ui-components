ac949207683756b22ad10a76dcb61570a0435655
baiocco <igor64br@gmail.com>
2025-07-27 20:57:51 -0300

    feat: radio share component

---
+++ src/app/home-page/components/RadioShare/style.module.css.d.ts
@@ -0,0 +1,15 @@
+declare const styles: {
+  readonly "body": string;
+  readonly "center": string;
+  readonly "discord": string;
+  readonly "instagram": string;
+  readonly "item": string;
+  readonly "messenger": string;
+  readonly "pinterest": string;
+  readonly "reddit": string;
+  readonly "snapchat": string;
+  readonly "twitter": string;
+  readonly "whatsapp": string;
+};
+export = styles;
+
--- src/app/home-page/page.tsx
+++ src/app/home-page/page.tsx
@@ -1,67 +1,104 @@
-import NeuComponent from "./components/NeuComponent/NeuComponent";
-import style from "./css/style.module.css";
-import ledCheckbox from "./css/led-checkbox.module.css";
-import topBottomButton from "./css/top-bottom-button.module.css";
-import squareSwitchButton from "./css/square-switch-button.module.css";
+import NeuComponent from './components/NeuComponent/NeuComponent';
+import style from './css/style.module.css';
+import ledCheckbox from './css/led-checkbox.module.css';
+import topBottomButton from './css/top-bottom-button.module.css';
+import squareSwitchButton from './css/square-switch-button.module.css';
 import fancyInput from './css/fancy-input.module.css';
-import LikeBtn from "./components/LikeBtn/LikeBtn";
-import "../globals.css";
+import '../globals.css';
+import LikeBtn from './components/LikeBtn/LikeBtn';
+import RadioShare from './components/RadioShare/RadioShare';
 
 export default function HomePage() {
-    return (
-        <div className={style.body}>
-            <h1>Neumorphic Components</h1>
-            <div className={style.componentContainer}>
-                <NeuComponent name="Square 3D Switch Button">
-                    {/* https://uiverse.io/njesenberger/rude-stingray-22 */}
-                    <div>
-                        <input type="checkbox" id={squareSwitchButton.squareSwitchId}/>
-                        <label htmlFor={squareSwitchButton.squareSwitchId}>
-                            <div className={squareSwitchButton.squareSwitchBody}>
-                                <div className={squareSwitchButton.squareSwitchLever}>
-                                    <div className={squareSwitchButton.switchLeverTexturePoint}></div>
-                                    <div className={squareSwitchButton.switchLeverTexturePoint}></div>
-                                    <div className={squareSwitchButton.switchLeverTexturePoint}></div>
-                                    <div className={squareSwitchButton.switchLeverTexturePoint}></div>
-                                    <div className={squareSwitchButton.switchLeverTexturePoint}></div>
-                                    <div className={squareSwitchButton.switchLeverTexturePoint}></div>
-                                    <div className={squareSwitchButton.switchLeverTexturePoint}></div>
-                                    <div className={squareSwitchButton.switchLeverTexturePoint}></div>
-                                    <div className={squareSwitchButton.switchLeverTexturePoint}></div>
-                                </div>
-                            </div>
-                        </label>
-                    </div>
-                </NeuComponent>
-                <NeuComponent name="Animated Like Button">
-                    {/* https://uiverse.io/LilaRest/loud-walrus-26 */}
-                    <LikeBtn/>
-                </NeuComponent>
-                <NeuComponent name="Top Bottom Button">
-                    {/* https://uiverse.io/ke1221/ancient-walrus-24 */}
-                    <button className={topBottomButton.topBottomButton} type="button">
-                        <p>Click me!</p>
-                    </button>
-                </NeuComponent>
-                <NeuComponent name="Switch Button">
-                    {/* https://uiverse.io/chase2k25/fluffy-dog-18 */}
-                    <div className={ledCheckbox.ledCheckboxContainer}>
-                        <input id={ledCheckbox.switchCheckbox} type="checkbox" name="switch-checkbox"/>
-                        <label id={ledCheckbox.switchCheckboxLabel} htmlFor={ledCheckbox.switchCheckbox}>
-                            <div className={ledCheckbox.movingLedContainer}>
-                                <div className={ledCheckbox.movingLed}></div>
-                            </div>
-                        </label>
-                    </div>
-                </NeuComponent>
-                <NeuComponent name="Fancy Text Input">
-                    {/* https://uiverse.io/AmIt-DasIT/weak-donkey-85 */}
-                    <div className={fancyInput.container}>
-                        <input type="text" name="text" id="text" className={fancyInput.input} required/>
-                        <label htmlFor="text" className={fancyInput.label}>Username</label>
-                    </div>
-                </NeuComponent>
-            </div>
-        </div>
-    );
+  return (
+    <div className={style.body}>
+      <h1>Neumorphic Components</h1>
+      <div className={style.componentContainer}>
+        <NeuComponent name="Square 3D Switch Button">
+          {/* https://uiverse.io/njesenberger/rude-stingray-22 */}
+          <div>
+            <input type="checkbox" id={squareSwitchButton.squareSwitchId} />
+            <label htmlFor={squareSwitchButton.squareSwitchId}>
+              <div className={squareSwitchButton.squareSwitchBody}>
+                <div className={squareSwitchButton.squareSwitchLever}>
+                  <div
+                    className={squareSwitchButton.switchLeverTexturePoint}
+                  ></div>
+                  <div
+                    className={squareSwitchButton.switchLeverTexturePoint}
+                  ></div>
+                  <div
+                    className={squareSwitchButton.switchLeverTexturePoint}
+                  ></div>
+                  <div
+                    className={squareSwitchButton.switchLeverTexturePoint}
+                  ></div>
+                  <div
+                    className={squareSwitchButton.switchLeverTexturePoint}
+                  ></div>
+                  <div
+                    className={squareSwitchButton.switchLeverTexturePoint}
+                  ></div>
+                  <div
+                    className={squareSwitchButton.switchLeverTexturePoint}
+                  ></div>
+                  <div
+                    className={squareSwitchButton.switchLeverTexturePoint}
+                  ></div>
+                  <div
+                    className={squareSwitchButton.switchLeverTexturePoint}
+                  ></div>
+                </div>
+              </div>
+            </label>
+          </div>
+        </NeuComponent>
+        <NeuComponent name="Animated Like Button">
+          {/* https://uiverse.io/LilaRest/loud-walrus-26 */}
+          <LikeBtn />
+        </NeuComponent>
+        <NeuComponent name="Top Bottom Button">
+          {/* https://uiverse.io/ke1221/ancient-walrus-24 */}
+          <button className={topBottomButton.topBottomButton} type="button">
+            <p>Click me!</p>
+          </button>
+        </NeuComponent>
+        <NeuComponent name="Switch Button">
+          {/* https://uiverse.io/chase2k25/fluffy-dog-18 */}
+          <div className={ledCheckbox.ledCheckboxContainer}>
+            <input
+              id={ledCheckbox.switchCheckbox}
+              type="checkbox"
+              name="switch-checkbox"
+            />
+            <label
+              id={ledCheckbox.switchCheckboxLabel}
+              htmlFor={ledCheckbox.switchCheckbox}
+            >
+              <div className={ledCheckbox.movingLedContainer}>
+                <div className={ledCheckbox.movingLed}></div>
+              </div>
+            </label>
+          </div>
+        </NeuComponent>
+        <NeuComponent name="Fancy Text Input">
+          {/* https://uiverse.io/AmIt-DasIT/weak-donkey-85 */}
+          <div className={fancyInput.container}>
+            <input
+              type="text"
+              name="text"
+              id="text"
+              className={fancyInput.input}
+              required
+            />
+            <label htmlFor="text" className={fancyInput.label}>
+              Username
+            </label>
+          </div>
+        </NeuComponent>
+        <NeuComponent name="Animated Share Wheel">
+          <RadioShare />
+        </NeuComponent>
+      </div>
+    </div>
+  );
 }
