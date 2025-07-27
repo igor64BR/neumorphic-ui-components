a5ac2b3c2e4682aa8c227d8ce8e8510ece89fc01
baiocco <igor64br@gmail.com>
2025-07-27 12:18:42 -0300

    refact: exibição inicial de home-page ao invés de index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function IndexPage() {
     useEffect(() => {
-        window.location.href = "/index";
+        window.location.href = "/home-page";
     }, []);
 
     return (
