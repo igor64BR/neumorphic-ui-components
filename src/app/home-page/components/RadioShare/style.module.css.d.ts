ea04110e33ccf22ee6c11c40ea876da484d52cde
baiocco <igor64br@gmail.com>
2025-07-27 22:04:49 -0300

    refact: RadioShare wheel has its options rendered from parameters

--- src/app/home-page/components/RadioShare/style.module.css.d.ts
+++ src/app/home-page/components/RadioShare/style.module.css.d.ts
@@ -1,15 +1,7 @@
 declare const styles: {
   readonly "body": string;
   readonly "center": string;
-  readonly "discord": string;
-  readonly "instagram": string;
   readonly "item": string;
-  readonly "messenger": string;
-  readonly "pinterest": string;
-  readonly "reddit": string;
-  readonly "snapchat": string;
-  readonly "twitter": string;
-  readonly "whatsapp": string;
 };
 export = styles;
 
